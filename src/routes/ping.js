2febbbace903069ab2176ef9530098cea2d74779
Calvin <zxw880507@gmail.com>
2021-09-18 17:19:45 -0600

    make change

--- src/routes/ping.js
+++ src/routes/ping.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 module.exports = (api_key = null) => {
   router.get("/", (req, res) => {
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, api_key });
   });
   return router;
 };
